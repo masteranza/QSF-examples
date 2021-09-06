168f65827f1e844aa2686a2f6ddfc121efc94052
Michal <masteranza@gmail.com>
2021-09-06 21:18:54 +0200

    —  both not exploited enough in many existing numerical frameworks

--- examples/template-ini/template-ini.cpp
+++ examples/template-ini/template-ini.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 {
 
 	QSF::init(argc, argv, project_dir / results_dir);
-	using grid_t = CartesianGrid<2_D>;
+	using grid_t = CartesianGrid<3_D>;
 	std::string im_output_name = "groundstate";
 
 
@@ -28,52 +28,57 @@ int main(int argc, char* argv[])
 		, CHANGE<SUM<AVG<PotentialEnergy>, AVG<KineticEnergy>>>
 	>;
 
-	auto p1 = QSF::SplitPropagator<MODE::IM, SplitType, im_wf_t>{};
-	p1.run<im_outputs_t>(
-		[&](const WHEN when, const ind step, const uind pass, auto& wf)
-		{
-			if (when == WHEN::AT_START)
+	if constexpr MODE_FILTER_OPT(MODE::IM)
+	{
+		auto p1 = QSF::SplitPropagator<MODE::IM, SplitType, im_wf_t>{};
+		p1.run<im_outputs_t>(
+			[&](const WHEN when, const ind step, const uind pass, auto& wf)
 			{
-				wf.addUsingCoordinateFunction(
-					[](auto... x) -> cxd
-					{
-						return cxd{ gaussian(0.0, 2.0, x...), 0 };
-					});
-				logUser("wf loaded manually!");
-			}
-			if (when == WHEN::AT_END)
-				im_output_name = wf.save(im_output_name);
-		});
+				if (when == WHEN::AT_START)
+				{
+					wf.addUsingCoordinateFunction(
+						[](auto... x) -> cxd
+						{
+							return cxd{ gaussian(0.0, 2.0, x...), 0 };
+						});
+					logUser("wf loaded manually!");
+				}
+				if (when == WHEN::AT_END)
+					wf.save(im_output_name);
+			});
+	}
 
 
+	if constexpr MODE_FILTER_OPT(MODE::RE)
+	{
+		using F1 = Field<AXIS::X, GaussianEnvelope<SinPulse>>;
+		using re_outputs_t = BufferedBinaryOutputs <
+			VALUE<Step, Time>
+			, VALUE<F1>
+			, AVG<Identity>
+			, AVG<PotentialEnergy>
+		// , PROJ<EIGENSTATES, Identity>
+			, AVG<DERIVATIVE<0, PotentialEnergy>>
+			// , FLUX<BOX<3>>
+			, VALUE<ETA> /* Estimated end of computation */
+		>;
+		using coupling_t = DipoleCoupling<VelocityGauge, F1>;
+		using re_wf_t = Schrodinger::Spin0<CAP<grid_t>, CoulombInteraction, coupling_t>;
 
-	using F1 = Field<AXIS::X, GaussianEnvelope<SinPulse>>;
-	using re_outputs_t = BufferedBinaryOutputs <
-		VALUE<Step, Time>
-		, VALUE<F1>
-		, AVG<Identity>
-		, AVG<PotentialEnergy>
-	// , PROJ<EIGENSTATES, Identity>
-		// , AVG<DERIVATIVE<0, PotentialEnergy>>
-		// , FLUX<BOX<3>>
-		, VALUE<ETA> /* Estimated end of computation */
-	>;
-	using coupling_t = DipoleCoupling<VelocityGauge, F1>;
-	using re_wf_t = Schrodinger::Spin0<CAP<grid_t>, CoulombInteraction, coupling_t>;
+		auto p2 = SplitPropagator<MODE::RE, SplitType, re_wf_t>{};
+		p2.run<re_outputs_t>(
+			[=](const WHEN when, const ind step, const uind pass, auto& wf)
+			{
+				if (when == WHEN::AT_START)
+					wf.load(im_output_name);
 
-	auto p2 = SplitPropagator<MODE::RE, SplitType, re_wf_t>{};
-	p2.run<re_outputs_t>(
-		[=](const WHEN when, const ind step, const uind pass, auto& wf)
-		{
-			if (when == WHEN::AT_START)
-				wf.load(im_output_name);
+				if (when == WHEN::DURING && step % 50 == 0)
+					wf.snapshot("step_" + std::to_string(step));
 
-			if (when == WHEN::DURING && step % 50 == 0)
-				wf.save("at" + std::to_string(step));
-
-			if (when == WHEN::AT_END)
-				wf.save("final");
-		});
+				if (when == WHEN::AT_END)
+					wf.save("final");
+			});
+	}
 
 	finalize();
 }
