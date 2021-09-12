4fbde4d67f172474bbd25b3713ea11d097dfdf39
Michal <masteranza@gmail.com>
2021-09-12 10:05:05 +0200

    improv

--- examples/neon-3e/prepare.sh
+++ examples/neon-3e/prepare.sh
@@ -7,7 +7,7 @@ shift;
 compressed=`echo $@ | tr -cd "[:alnum:]"`
 newname="${template}-${compressed}.sh"
 change=$@ #`echo "$@"`
-echo "Switching [PARAMS]->[$change] and [COMPRESSED]->[$change], saved in ./${newname}"
+echo "Switching [PARAMS]->[$change] and [COMPRESSED]->[$compressed], saved in ./${newname}"
 sed "s/PARAMS/$change/g" ${org} | sed "s/COMPRESSED/$compressed/g" > ${newname}
 #  ${newname}  > ${newname}
 chmod u+x ${newname}
